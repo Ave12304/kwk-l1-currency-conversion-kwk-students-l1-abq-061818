0d979cc835e8efd98097e04e62976529581cd253
Ave12304 <averyselover@hotmail.com>
2018-06-22 16:21:08 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,7 +1,7 @@
-def usd_to_eur = (10)
-  puts 10 usd = 8.59 euros
+def usd_to_eur = (usd)
+  puts "10 usd = 8.59 euros"
 end
 #1 united states dollar = 0.86 euro
-def eur_to_usd = (8.59)
-  puts 8.59 euros = 10 usd
+def eur_to_usd = (eur)
+  puts "8.59 euros = 10 usd"
 end
